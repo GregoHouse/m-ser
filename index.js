271036d76201c1eeac977fa16d0e69e7b1c206b6
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-06-08 17:11:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const server = require("./src/app.js");
+// const { conn } = require("./src/db.js");
+require("dotenv").config();
+const { conn } = require("./src/database/db.js");
+const { PORT } = process.env;
+const server = require("./src/app.js");
+
+conn.sync({ alter: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`server listening at ${PORT}`);
+  });
+});
