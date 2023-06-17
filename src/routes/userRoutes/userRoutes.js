110394d998d4841865f5834809d045141c19626b
jkapa7 <juandaniel5005@gmail.com>
2023-06-16 21:22:44 -0500

    Agregue la ruta /users para traer todos los users

--- src/routes/userRoutes/userRoutes.js
+++ src/routes/userRoutes/userRoutes.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const { createUserController } = require("../../controllers/userController");
+const {
+  getUsersController,
+} = require("../../controllers/userController/getUsersController");
 
 const userRoutes = Router();
 
 // userRoutes.post("/", createUserController);
+
+userRoutes.get("/users", getUsersController);
 
 module.exports = userRoutes;
