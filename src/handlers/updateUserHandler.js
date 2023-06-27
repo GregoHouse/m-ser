979b56668d65c8945d5c8470fcdcc538842ba028
XCODE89 <maph0989@gmail.com>
2023-06-27 11:29:04 -0400

    Ruta para editar perfil - MAponte

---
+++ src/handlers/updateUserHandler.js
@@ -0,0 +1,14 @@
+const {updateUserController} = require("../controllers/userController/updateUserController");
+  
+  const updateUserHandler = async (req, res) => {
+    
+    try {
+      const updatedUser = await updateUserController(req);
+      return res.status(200).json(updatedUser);
+    } catch (error){
+      console.log(error.message);
+      return res.status(400).json({ message: error.message });
+    }
+  };
+  
+  module.exports = { updateUserHandler };
--- src/routes/userRoutes/userRoutes.js
+++ src/routes/userRoutes/userRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const userHandler = require("../../handlers/userHandler.js");
 const { createUserHandler } = require("../../handlers/createUserHandler");
+const { updateUserHandler } = require("../../handlers/updateUserHandler")
 const userRoutes = Router();
 
 userRoutes.post("/", createUserHandler);
 userRoutes.get("/", userHandler);
+userRoutes.put("/update/:id_user", updateUserHandler);
 
 
 module.exports = userRoutes;
