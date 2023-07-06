75b04d1b281ed23e2f3269d5fbb8ad437f90e171
jkapa7 <juandaniel5005@gmail.com>
2023-07-06 14:08:02 -0500

    Route to bring users by id

--- src/routes/userRoutes/userRoutes.js
+++ src/routes/userRoutes/userRoutes.js
@@ -1,14 +1,17 @@
 const { Router } = require("express");
-const userHandler = require("../../handlers/userHandler.js");
+const {
+  userHandler,
+  getUserByIdHandler,
+} = require("../../handlers/userHandler.js");
 const { createUserHandler } = require("../../handlers/createUserHandler");
-const { updateUserHandler } = require("../../handlers/updateUserHandler")
+const { updateUserHandler } = require("../../handlers/updateUserHandler");
 const { createUserValidation } = require("../../utils/middlware");
 
 const userRoutes = Router();
 
 userRoutes.post("/", createUserValidation, createUserHandler);
 userRoutes.get("/", userHandler);
+userRoutes.get("/", getUserByIdHandler);
 userRoutes.put("/update/:id_user", updateUserHandler);
-
 
 module.exports = userRoutes;
