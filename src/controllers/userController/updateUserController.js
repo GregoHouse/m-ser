1a878f218754bfd40d4f11b2973c494b4d7352c9
EsteChar2906 <ccarlospachecoc@gmail.com>
2023-07-10 10:42:49 -0500

    Relacion User-Sport

--- src/controllers/userController/updateUserController.js
+++ src/controllers/userController/updateUserController.js
@@ -1,40 +1,57 @@
-const {cloudiconfig, loadPhoto, DeletePhoto} = require("../../utils/cloudinary");
-const { User } = require("../../db");
-  
-  const updateUserController = async (req) => {
-    const { id_user } = req.params;
-    console.log(req.body)
-    console.log(req.params)
-    let { body } = req;
-    
-    if (!id_user) {
-      throw new Error("No se especificó el ID del usuario");
-    } else {
-      body.id_user = parseInt(id_user);
-  
-      const user = await User.findOne({
-        where: { id_user },
-      });
-      if (!user) throw new Error("No se encontró ningún usuario con ese ID");
+const {
+  cloudiconfig,
+  loadPhoto,
+  DeletePhoto,
+} = require("../../utils/cloudinary");
+const { User, Sport } = require("../../db");
 
-      if (req.files) {
-        const { avatar_img } = req.files;
-        cloudiconfig();
-        if (avatar_img) {
-          if (user.avatar_img) await DeletePhoto(user.avatar_img);
-          const UpdateAvatarImg  = await loadPhoto(avatar_img.tempFilePath,"User",user.email);
-          body.avatar_img = UpdateAvatarImg.secure_url;
-        } else {
-          body.avatar_img = user.avatar_img;
-        }
+const updateUserController = async (req) => {
+  const { id_user } = req.params;
+  console.log(req.body);
+  console.log(req.params);
+  let { body } = req;
+  let { sports } = body;
+  if (!id_user) {
+    throw new Error("No se especificó el ID del usuario");
+  } else {
+    body.id_user = parseInt(id_user);
+
+    const user = await User.findOne({
+      where: { id_user },
+    });
+    if (!user) throw new Error("No se encontró ningún usuario con ese ID");
+
+    if (req.files) {
+      const { avatar_img } = req.files;
+      cloudiconfig();
+      if (avatar_img) {
+        if (user.avatar_img) await DeletePhoto(user.avatar_img);
+        const UpdateAvatarImg = await loadPhoto(
+          avatar_img.tempFilePath,
+          "User",
+          user.email
+        );
+        body.avatar_img = UpdateAvatarImg.secure_url;
       } else {
         body.avatar_img = user.avatar_img;
       }
-      await User.update(body, { where: { id_user: parseInt(id_user) } });
+    } else {
+      body.avatar_img = user.avatar_img;
     }
-    const userActualizado = await User.findOne({
-      where: {id_user}
+    await User.update(body, { where: { id_user: parseInt(id_user) } });
+  }
+  const userActualizado = await User.findOne({
+    where: { id_user },
+  });
+
+  sports &&
+    sports.length > 0 &&
+    sports.forEach(async (sport) => {
+      const sportRelation = await Sport.findOne({ where: { id_sport: sport } });
+
+      sportRelation.addUsers(userActualizado);
     });
+
   const newInfoUser = {
     id_user: userActualizado.id_user,
     name: userActualizado.name.toUpperCase(),
