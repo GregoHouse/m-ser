7d5da3574d96d6f650c2b86d90d04686c8b5cab3
EsteChar2906 <ccarlospachecoc@gmail.com>
2023-07-11 17:36:03 -0500

    Agregando id y email a la respuesta delogin

--- src/controllers/userController/loginUserController.js
+++ src/controllers/userController/loginUserController.js
@@ -1,19 +1,24 @@
-require('dotenv').config();
-const { User } = require('../../db');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+require("dotenv").config();
+const { User } = require("../../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const { DB_NAME } = process.env;
 
 const loginUser = async (email, password) => {
-    const user = await User.findOne({ where: { email }});
-    const validatePassword = await bcrypt.compare(password, user.password);
+  const user = await User.findOne({ where: { email } });
+  const validatePassword = await bcrypt.compare(password, user.password);
 
-    if(validatePassword) {
-        return jwt.sign({ email }, DB_NAME , { expiresIn: '24h'});    
-    }
+  if (user && validatePassword) {
+    let response = {
+      id: user.id_user,
+      email: user.email,
+      name: `${user.firstname} ${user.lastname}`,
+      token: jwt.sign({ email }, DB_NAME, { expiresIn: "24h" }),
+    };
 
-    return false
-
-}
+    return response;
+  }
+  return false;
+};
 
 module.exports = loginUser;
